a4465c87f6f889a7bcdf2fca009c325101592c93
Lola <lolahef@gmail.com>
2019-03-27 17:42:45 -0400

    Finished basic hashtables; passing all tests

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -70,8 +70,9 @@ unsigned int hash(char *str, int max)
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
-  BasicHashTable *ht;
-
+  BasicHashTable *ht = malloc(sizeof(BasicHashTable));
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair));
   return ht;
 }
 
@@ -84,7 +85,14 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
-
+  unsigned int index = hash(key, ht->capacity);
+  if (ht->storage[index] != NULL) {
+    printf("overwriting file");
+    destroy_pair(ht->storage[index]);
+    ht->storage[index] = create_pair(key, value);
+  } else {
+    ht->storage[index] = create_pair(key, value);
+  }
 }
 
 /****
@@ -94,7 +102,9 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
-
+  unsigned int index = hash(key, ht->capacity);
+  ht->storage[index] = NULL;
+  destroy_pair(ht->storage[index]);
 }
 
 /****
@@ -104,7 +114,12 @@ void hash_table_remove(BasicHashTable *ht, char *key)
  ****/
 char *hash_table_retrieve(BasicHashTable *ht, char *key)
 {
-  return NULL;
+  unsigned int index = hash(key, ht->capacity);
+  if (ht->storage[index] != NULL) {
+    return ht->storage[index]->value;
+  } else {
+    return NULL;
+  }
 }
 
 /****
@@ -114,7 +129,11 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
-
+  for (int i = 0; i < ht->capacity; i++) {
+    free(ht->storage[i]);
+  }
+  free(ht->storage);
+  free(ht);
 }
 
 
